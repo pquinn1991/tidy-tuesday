ea52cdfa62e3a419d937390a60af64214f35d7f1
Parker Quinn <pquinn91@gmail.com>
2019-02-06 16:43:09 -0600

    housing update with change year over year

--- hpi-mortgage 2-5-19/hpi_mortgage.R
+++ hpi-mortgage 2-5-19/hpi_mortgage.R
@@ -23,3 +23,25 @@ p <- usa_hpi %>%
         transition_time(year)
 
 animate(p, fps = 3)
+
+## Excursion -- can we look at the change year-over-year instead?
+pct <- function(x) {((x/lag(x))-1)*100}
+
+usa_hpi_change <- usa_hpi %>% 
+  group_by(state_abbr, year) %>% 
+  filter(month == 1) %>% 
+  mutate(change = price_index) %>% 
+  group_by(state_abbr) %>% 
+  mutate_each(funs(pct), change)
+
+p2 <- usa_hpi_change %>%
+  ggplot() + 
+  geom_sf(aes(fill = change)) + 
+  scale_fill_distiller(name = "Percent change \nfrom previous year", palette = "RdBu", direction = 1) + 
+  coord_sf(crs = 5070) + 
+  theme_void() + 
+  theme(panel.grid = element_line(color = 'white')) + 
+  labs(title = "House Price Index in {frame_time}", caption = "Data source:  Freddie Mac House Price Index (State and National)") + 
+  transition_time(year)
+
+animate(p2, fps = 3)
