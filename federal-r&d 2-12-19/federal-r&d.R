08d3b0cbdb87703332ebe0cc9a8adec260cf37a4
Parker Quinn <pquinn91@gmail.com>
2019-02-13 20:53:19 -0600

    tidy tuesday update

--- federal-r&d 2-12-19/federal-r&d.R
+++ federal-r&d 2-12-19/federal-r&d.R
@@ -1,5 +1,8 @@
 library(RColorBrewer)
 library(tidyverse)
+library(grid)
+library(gridExtra)
+library(scales)
 
 # Read data
 fed_rd <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/fed_r_d_spending.csv")
@@ -35,7 +38,7 @@ fed_rd_combine %>%
   ggplot(aes(x = year, y = rd_perc, fill = department2)) + 
     geom_area(color = "white", size = 0.15, alpha = 0.7) + 
     scale_fill_manual(values = c("grey", brewer.pal(7, "Dark2"))) + 
-    scale_y_continuous(labels = scales::percent) + 
+    scale_y_continuous(labels = percent) + 
     theme_minimal() + 
     labs(x = "\nFiscal Year", 
          y = "Federal Discretionary Spending", 
@@ -61,15 +64,48 @@ fed_rd_combine %>%
 energy_spend <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/energy_spending.csv")
 climate_spend <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/climate_spending.csv")
 
-fed_avg <- fed_rd_combine %>% filter(year >= 2000, department != "Non-R&D") %>% group_by(department, year) %>% summarise(rd_budget = min(rd_budget)) %>% mutate(changePct = (rd_budget/lag(rd_budget))-1) %>% mutate(changePct = ifelse(is.infinite(changePct), NA, changePct)) %>% group_by(department) %>% summarise(rd_avg = mean(rd_budget, na.rm = TRUE), change_avg = mean(changePct, na.rm = TRUE), type = "Other R&D")
+fed_delta <- fed_rd_combine %>% filter(year >= 2000, department != "Non-R&D") %>% group_by(year) %>% summarise(rd_budget = sum(rd_budget)) %>% mutate(changePct = (rd_budget/lag(rd_budget))-1) %>% mutate(changePct = ifelse(is.infinite(changePct), NA, changePct), type = "Other")
+climate_delta <- climate_spend %>% group_by(year) %>% summarise(rd_budget = sum(gcc_spending)) %>% mutate(changePct = (rd_budget/lag(rd_budget))-1) %>% mutate(changePct = ifelse(is.infinite(changePct), NA, changePct), type = "Climate")
+energy_delta <- energy_spend %>% filter(year >= 2000) %>% group_by(year) %>% summarise(rd_budget = sum(energy_spending)) %>% mutate(changePct = (rd_budget/lag(rd_budget))-1) %>% mutate(changePct = ifelse(is.infinite(changePct), NA, changePct), type = "Energy")
+rd_deltas <- rbind(fed_delta, climate_delta, energy_delta)
 
-climate_avg <- climate_spend %>% group_by(department, year) %>% summarise(rd_budget = min(gcc_spending)) %>% mutate(changePct = (rd_budget/lag(rd_budget))-1) %>% mutate(changePct = ifelse(is.infinite(changePct), NA, changePct)) %>% group_by(department) %>% summarise(rd_avg = mean(rd_budget, na.rm = TRUE), change_avg = mean(changePct, na.rm = TRUE), type = "Climate R&D")
+budget_candlestick <- rd_deltas %>% 
+  ggplot(aes(x = type, y = rd_budget, color = type)) + 
+    geom_boxplot() + 
+    scale_y_continuous(labels = dollar_format(scale = 1/1000000000)) + 
+    theme_minimal() + 
+    theme(legend.position = "none", 
+          plot.title = element_text(face = "bold", size = 12), 
+          axis.title = element_text(face = "bold"), 
+          axis.text = element_text(size = 10, face = "italic"), 
+          axis.text.x = element_text(size = 10, face = "bold.italic"), 
+          panel.grid.minor = element_blank()) + 
+    labs(x = "", y = "", title = "Annual spending on R&D")
 
-energy_avg <- energy_spend %>% filter(year >= 2000) %>% group_by(department, year) %>% summarise(rd_budget = min(energy_spending)) %>% mutate(changePct = (rd_budget/lag(rd_budget))-1) %>% mutate(changePct = ifelse(is.infinite(changePct), NA, changePct)) %>% group_by(department) %>% summarise(rd_avg = mean(rd_budget, na.rm = TRUE), change_avg = mean(changePct, na.rm = TRUE), type = "Energy R&D")
+change_candlestick <- rd_deltas %>% 
+  ggplot(aes(x = type, y = changePct, color = type)) + 
+  geom_boxplot() + 
+  scale_y_continuous(labels = scales::percent) + 
+  theme_minimal() + 
+  theme(legend.position = "none", 
+        plot.title = element_text(face = "bold", size = 12), 
+        axis.title = element_text(face = "bold"), 
+        axis.text = element_text(size = 10, face = "italic"), 
+        axis.text.x = element_text(size = 10, face = "bold.italic"), 
+        panel.grid.minor = element_blank()) + 
+  labs(x = "", y = "", title = "Year-to-year change")
 
-rd_avg <- rbind(fed_avg, climate_avg, energy_avg)
+grid.arrange(budget_candlestick, 
+             change_candlestick, 
+             nrow = 1, 
+             bottom = textGrob(
+               "Data from 2000-2017",
+               gp = gpar(fontface = 3, fontsize = 9),
+               hjust = 1,
+               x = 1), 
+             top = textGrob("Climate and Energy R&D are way behind...\nand they aren't catching up any time soon.\n", gp = gpar(fontface = 4, fontsize = 16)))
 
-rd_avg %>% ggplot(aes(x = log(rd_avg), y = change_avg, color = type)) + geom_point()
-rd_avg %>% ggplot(aes(x = 1, y = change_avg, color = type)) + geom_point(aes(size = rd_avg))
-rd_avg %>% ggplot(aes(y = rd_avg, color = type)) + geom_boxplot()
-rd_avg %>% ggplot(aes(y = change_avg, color = type)) + geom_boxplot()
+
+
+
+
